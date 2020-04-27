08e33c2557944f6e85f8fe00c3660063fe931467
atav1k <atavikram@gmail.com>
2020-04-27 09:54:21 -0600

    alt tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,10 +19,10 @@ img {
 
 <ul grid columns="4" gap="8">
   <li cell><h4>Collection</h4></li>
-  <li cell><img src="https://s3.us-east-2.wasabisys.com/postlook/DSC01027.JPG"></li>
-  <li cell><img src="https://s3.us-east-2.wasabisys.com/postlook/DSC01028.JPG"></li>
-  <li cell><img src="https://s3.us-east-2.wasabisys.com/postlook/DSC01030.JPG"></li>
-  <li cell><img src="https://s3.us-east-2.wasabisys.com/postlook/DSC01031.JPG"></li>
-  <li cell><img src="https://s3.us-east-2.wasabisys.com/postlook/DSC01032.JPG"></li>
-  <li cell><img src="https://s3.us-east-2.wasabisys.com/postlook/DSC01033.JPG"></li>
+  <li cell><img alt="photo" src="https://s3.us-east-2.wasabisys.com/postlook/DSC01027.JPG"></li>
+  <li cell><img alt="photo" src="https://s3.us-east-2.wasabisys.com/postlook/DSC01028.JPG"></li>
+  <li cell><img alt="photo" src="https://s3.us-east-2.wasabisys.com/postlook/DSC01030.JPG"></li>
+  <li cell><img alt="photo" src="https://s3.us-east-2.wasabisys.com/postlook/DSC01031.JPG"></li>
+  <li cell><img alt="photo" src="https://s3.us-east-2.wasabisys.com/postlook/DSC01032.JPG"></li>
+  <li cell><img alt="photo" src="https://s3.us-east-2.wasabisys.com/postlook/DSC01033.JPG"></li>
 </ul>
